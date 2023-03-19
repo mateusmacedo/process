bd0482bf6d644b7269ba80e0857454cb5b8ffeaa
Mateus Anjos <mateus.anjos@fretebras.com.br>
2023-03-19 18:49:56 -0300

    chore: stoplight files

---
+++ docs/.spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MDY4OA/spectral.js?branch_slug=main&token=2a8b0365-f3cc-433f-9fd8-502f8078bf2c";
+export default { extends: ruleset };
